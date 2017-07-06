7ec87e13c08d1e4938a33b9cd122a5ebb01a3f43
phix <pokphoenix@gmail.com>
2017-07-06 21:26:23 +0700

    cut admin register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,6 +38,8 @@
                         {{ config('app.name', 'Laravel') }}
                     </a>
                 </div>
+
+               
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                    
                     <ul class="nav navbar-nav">
@@ -47,9 +49,11 @@
                 
                     <ul class="nav navbar-nav navbar-right">
                        
-                        @if (Auth::guest())
+                        @if (Auth::guest() )
                             <li><a href="{{ route('login') }}">Login</a></li>
+                            @if ( Route::currentRouteName() != "admin.login" )
                             <li><a href="{{ route('register') }}">Register</a></li>
+                            @endif
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
